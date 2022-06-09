fb38f6d87cfc8d9ed6d0fa37f9f8f11f4c409c18
nabilahdyh <nabilahidayah.nh@gmail.com>
2022-06-09 23:47:27 +0700

    Update Index.js with new chall

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, data) => {
+  let datas = [];
+  let a = data(names);
+  a.forEach((value, key) => {
+    datas.push(`${key + 1}. ${value}`);
+  });
+  return datas;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => data.sort();
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => data.reverse();
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
